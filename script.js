9d85ea8f39720f2e081f7713c00a744660699677
Ankur Kumar <98178905+Ankur-Kumar-4@users.noreply.github.com>
2023-09-15 13:12:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
-    //Write your code here
+
+	var firstName = document.getElementById("namef").value;
+            var lastName = document.getElementById("namel").value;
+
+            // Display the values in an alert
+            alert(firstName +" " +lastName);
 
 }
